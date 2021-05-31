c4330218f8b2ad4a5373c2f188fc07a67e191c39
JakubKivi <jakub.kivi.kuziemski@gmail.com>
2021-06-01 00:54:06 +0200

    added blur

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ function createArray(length) {
 
 var p= createArray(canvas.width, canvas.height);    //0-tlo; 1-kropeczka
 var p2= createArray(canvas.width, canvas.height);
+var ps= createArray(canvas.width, canvas.height);
 
 function drawPixel (x, y, r, g, b, a) {
     var index = (x + y * canvasWidth) * 4;
@@ -68,7 +69,7 @@ function drawPixels() {
     for(var i=0; i<canvas.width; i++){
         for(var j=0; j<canvas.height; j++){
             
-            if(p[i][j]==1)drawPixel(i,j, r,g,b,255);
+            if(ps[i][j])drawPixel(i,j, r+ps[i][j],g+ps[i][j]/2,b+ps[i][j],255);
         }
     }
 }
@@ -110,6 +111,71 @@ function gameoflife() {
     }
 }
 
+function psalg(a) {
+    if(a==2){
+        for(var i=2; i<canvas.width-2; i++){
+            for(var j=2; j<canvas.height-2; j++){
+                var s=0;
+                if(p[i][j])s++;
+                if(p[i+1][j])s++;
+                if(p[i-1][j])s++;
+                if(p[i][j+1])s++;
+                if(p[i][j-1])s++;
+
+                if(p[i+2][j])s++;
+                if(p[i-2][j])s++;
+                if(p[i][j+2])s++;
+                if(p[i][j-2])s++;
+
+                if(p[i+1][j+1])s++;
+                if(p[i+1][j-1])s++;
+                if(p[i-1][j+1])s++;
+                if(p[i-1][j-1])s++;
+
+                if(p[i+2][j+1])s++;
+                if(p[i+2][j-1])s++;
+                if(p[i-2][j+1])s++;
+                if(p[i-2][j-1])s++;
+
+                if(p[i+1][j+2])s++;
+                if(p[i+1][j-2])s++;
+                if(p[i-1][j+2])s++;
+                if(p[i-1][j-2])s++;
+
+                if(p[i+2][j+2])s++;
+                if(p[i+2][j-2])s++;
+                if(p[i-2][j+2])s++;
+                if(p[i-2][j-2])s++;
+                ps[i][j]=s/25*255;
+            }
+        }
+    }else if(a==1){
+        for(var i=2; i<canvas.width-2; i++){
+            for(var j=2; j<canvas.height-2; j++){
+                var s=0;
+                if(p[i][j])s++;
+                if(p[i+1][j])s++;
+                if(p[i-1][j])s++;
+                if(p[i][j+1])s++;
+                if(p[i][j-1])s++;
+
+                if(p[i+1][j+1])s++;
+                if(p[i+1][j-1])s++;
+                if(p[i-1][j+1])s++;
+                if(p[i-1][j-1])s++;
+                ps[i][j]=s/9*255;
+            }
+        }
+    }else{
+        for(var i=2; i<canvas.width-2; i++){
+            for(var j=2; j<canvas.height-2; j++){
+                if(p[i][j])ps[i][j]=255;
+                else ps[i][j]=0;
+            }
+        }
+    }
+}
+
 function start(){
 
 
@@ -120,7 +186,7 @@ function start(){
         }
     }
 
-    for(var i=0; i<(canvasWidth*canvasHeight)/6;i++){
+    for(var i=0; i<(canvasWidth*canvasHeight)/5;i++){
         var a = Math.floor(Math.random() * (canvasWidth-2))+1;
         var b = Math.floor(Math.random() * (canvasHeight-2))+1;
         if(p[a][b]==1)i--;
@@ -141,6 +207,8 @@ function update(){
     fillWhole(r1,g1,b1,255);
 
     gameoflife();
+
+    psalg(2);
 
     drawPixels();
     updateCanvas();   
